06db7ef6aaad3f84980d00d9129d966a65b884ea
dharmeshrao <yadavdydv@gmail.com>
2021-12-23 17:26:34 +0530

    abc

--- Matrix/3dMatrix.js
+++ Matrix/3dMatrix.js
@@ -1,35 +1,33 @@
 function runProgram(input) {
-   input = input.trim().split('\n')
-   let [n,r,c] = input[0].trim().split(' ').map(Number);
-   let array = input[1].trim().split(' ').map(Number);
-   let res = []
-   for(let i = 0;i< n;i++){
-   let mat = []
-   for(let j = 0;j < array.length;j++){
-       console.log(array[j]);
-   }
-   }
+  input = input.trim().split("\n");
+  let [n, r, c] = input[0].trim().split(" ").map(Number);
+  let array = input[1].trim().split(" ").map(Number);
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    let mat = [];
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[j]);
+    }
   }
-  if (process.env.USERNAME === "adam") {
-    runProgram(`2 2 2
+}
+if (process.env.USERNAME === "adam") {
+  runProgram(`2 2 2
     1 2 3 4 5 6 7 8`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
-  
-  
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
