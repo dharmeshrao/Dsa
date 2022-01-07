3fd9c2bd506a9b97a82c93adae9e39f365d2997d
Dharmesh <yadavdydv@gmail.com>
2022-01-07 13:19:47 +0530

    updaete

---
+++ FlatAnArray.js
@@ -0,0 +1,3 @@
+const biDimensionalArr = [11, [22, 33], [44, 55], [66, 77], 88, 99];
+const flattenArr = [].concat(...biDimensionalArr); // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+console.log(flattenArr);
