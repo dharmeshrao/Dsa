20dea65720c72f9d775990caf80624804c6ba260
Dharmesh <yadavdydv@gmail.com>
2022-01-08 20:43:39 +0530

    update

--- AgainStartingFromBasic/sumOfSub.js
+++ AgainStartingFromBasic/sumOfSub.js
@@ -1,48 +1,44 @@
-const runProgram = (input)=> {
-    input = input.trim().split('\n');
-    let arr = input[1].trim().split(" ").map(Number);
-    let ans = [];
-    let sum = 0;
-    for(let i = 0; i < arr.length;  i++){
-        sum+=arr[i];
-        ans.push(sum);
-    }
-   for(let i = 3; i < input.length; i++){
-       let [l,r] = input[i].trim().split(" ").map(Number);
-       if(l===0){
-        //    console.log(ans[r]);
-       }else{
-        //    console.log("abc",ans[l-1-1])
-           console.log(ans[r] - ans[l-1]);
-           console.log(ans)
-       }
-   }
-     
-     
-    }
-    if (process.env.USERNAME === "Dharmesh") {
-      runProgram(`4
-      3 2 1 5
-      4
-      0 2
-      1 3
-      0 3
-      2 2`);
+const runProgram = (input) => {
+  input = input.trim().split("\n");
+  let arr = input[1].trim().split(" ").map(Number);
+  let ans = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    ans.push(sum);
+  }
+  for (let i = 3; i < input.length; i++) {
+    let [l, r] = input[i].trim().split(" ").map(Number);
+    if (l === 1) {
+      console.log(ans[r - 1]);
     } else {
-      process.stdin.resume();
-      process.stdin.setEncoding("ascii");
-      let read = "";
-      process.stdin.on("data", function (input) {
-        read += input;
-      });
-      process.stdin.on("end", function () {
-        read = read.replace(/\n$/, "");
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-      });
-      process.on("SIGINT", function () {
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-        process.exit(0);
-      });
+      console.log(ans[r - 1] - ans[l - 1 - 1]);
     }
+  }
+};
+if (process.env.USERNAME === "Dharmesh") {
+  runProgram(`4
+  3 2 1 5
+  4
+  1 3
+  2 4
+  1 4
+  3 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
--- Matrix/Ltraversel.js
+++ Matrix/Ltraversel.js
@@ -1,24 +1,38 @@
-const findit = (n) => {
-  let res = "";
-  while (0 < n) {
-    res += n % 2;
-    n = Math.floor(n / 2);
+function traverseLshape(mat, r, c) {
+  let res = "",
+    j = 0;
+  while (j < c) {
+    for (var i = 0; i <= r - j - 1; i++) res += mat[i][j] + " ";
+    for (var k = j + 1; k < c; k++) res += mat[r - 1 - j][k] + " ";
+    j++;
   }
-  console.log(res.split("").reverse().join(""));
-};
+  console.log(res);
+}
+
 const runProgram = (input) => {
   input = input.trim().split("\n");
-  let cases = +input[0],
-    line = 1;
+  let cases = +input[0];
+  let line = 1;
   for (let i = 0; i < cases; i++) {
-    let n = +input[line++];
-    findit(n);
+    let [r, c] = input[line++].trim().split(" ").map(Number);
+    let mat = [];
+    for (let j = 0; j < r; j++) {
+      mat.push(input[line++].trim().split(" ").map(Number));
+    }
+    traverseLshape(mat, r, c);
   }
 };
 if (process.env.USERNAME === "Dharmesh") {
   runProgram(`2
-    15
-    128`);
+    3 3
+    1 2 3
+    4 5 6
+    7 8 9
+    4 3
+    1 2 3
+    4 5 6
+    7 8 9
+    10 11 12`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
