265032f0fb7787c0f90896ac1406f90175181753
Dharmesh <yadavdydv@gmail.com>
2022-01-16 21:54:15 +0530

    weighted sum

---
+++ LastRevision/weightedSum.js
@@ -0,0 +1,39 @@
+const findWeightedSum = (array, n) => {
+  let sum = 0;
+  for (let i = 0; i < n; i++) sum += array[i] * (i + 1);
+  console.log(sum);
+};
+const runProgram = (input) => {
+  input = input.trim().split("\n");
+  let cases = +input[0],
+    line = 1;
+  for (let i = 0; i < cases; i++) {
+    let n = +input[line++],
+      array = input[line++].trim().split(" ").map(Number);
+    findWeightedSum(array, n);
+  }
+};
+if (process.env.USERNAME === "Dharmesh") {
+  runProgram(`2
+    5
+    1 2 3 4 5
+    2
+    100 1`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
