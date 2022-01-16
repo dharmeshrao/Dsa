b221e6e04ccc444373c66e46759a4a8fbc828c7c
Dharmesh <yadavdydv@gmail.com>
2022-01-16 15:14:32 +0530

    update

--- AgainStartingFromBasic/countSuchPairs.js
+++ AgainStartingFromBasic/countSuchPairs.js
@@ -1,27 +1,39 @@
-const runProgram = (input)=> {
-  input = input.trim().split('')
-   
-   
+const runProgram = (input) => {
+  input = input.trim().split("\n");
+  let [n, k] = input[0].trim().split(" ").map(Number);
+  let array = input[1]
+    .trim()
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+  let count = 0,
+    i = 0,
+    j = n - 1;
+  while (i < j) {
+    if (array[i] + array[j] == k) count++;
+    if (array[i] + array[j] > k) j--;
+    else i++;
   }
-  if (process.env.USERNAME === "Dharmesh") {
-    runProgram(``);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
-  
-  
+  console.log(count);
+};
+if (process.env.USERNAME === "Dharmesh") {
+  runProgram(`5 9
+    3 0 6 2 7`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
