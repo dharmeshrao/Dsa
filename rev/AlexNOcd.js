ebf6777f70daae395ade45bf978db83190aca234
Dharmesh <yadavdydv@gmail.com>
2022-01-19 10:31:12 +0530

    updaet

---
+++ rev/AlexNOcd.js
@@ -0,0 +1,44 @@
+const findOcd = (array, n) => {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((array[i] + array[j]) % 60 === 0) count++;
+    }
+  }
+  console.log(count);
+};
+
+const runProgram = (input) => {
+  input = input.trim().split("\n");
+  let cases = +input[0],
+    line = 1;
+  for (let i = 0; i < cases; i++) {
+    let n = +input[line++],
+      array = input[line++].trim().split(" ").map(Number);
+    findOcd(array, n);
+  }
+};
+if (process.env.USERNAME === "Dharmesh") {
+  runProgram(`2
+    3
+    60 60 60
+    5
+    30 30 10 20 400`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
---
+++ rev/nickNHack.js
@@ -0,0 +1,40 @@
+const findCoins = (n, k) => {
+  if (n === k) return true;
+  if (n > k) return false;
+  return findCoins(n * 10, k) || findCoins(n * 20, k);
+};
+
+const runProgram = (input) => {
+  input = input.trim().split("\n");
+  let cases = +input[0],
+    line = 1;
+  for (let i = 0; i < cases; i++) {
+    let n = +input[line++];
+    findCoins(1, n) ? console.log("Yes") : console.log("No");
+  }
+};
+if (process.env.USERNAME === "Dharmesh") {
+  runProgram(`5
+    1
+    2
+    10
+    25
+    200`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
