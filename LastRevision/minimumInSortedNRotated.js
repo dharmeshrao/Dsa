988efce6073edac686c85a9c34434f23ea4605ad
Dharmesh <yadavdydv@gmail.com>
2022-01-20 14:59:51 +0530

    update

--- LastRevision/minimumInSortedNRotated.js
+++ LastRevision/minimumInSortedNRotated.js
@@ -1,15 +1,21 @@
 const findMin = (array, n) => {
   let low = 0,
-    high = n - 1;
-  if (array[0] <= array[n - 1]) return array[0];
+    high = n - 1,
+    res = 0;
   while (low <= high) {
     let mid = Math.floor(low + (high - low) / 2);
-    if (array[mid] > array[mid + 1]) return array[mid + 1];
+    if (array[mid] > array[mid + 1]) {
+      return array[mid + 1];
+    }
     if (array[mid] < array[mid - 1]) return array[mid];
-    if (array[mid] > array[0]) low = mid + 1;
-    else high = mid - 1;
+    if (array[mid] > array[0]) {
+      low = mid + 1;
+    } else {
+      res = array[mid - 1];
+      high = mid - 1;
+    }
   }
-  return -1;
+  return res;
 };
 
 const runProgram = (input) => {
