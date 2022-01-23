3d4396a38b82f9c8f6882749e0dd46825370f41f
Dharmesh <yadavdydv@gmail.com>
2022-01-23 08:32:54 +0530

    update

---
+++ StockSpan.js
@@ -0,0 +1,45 @@
+const findStock = (array,n)=>{
+    let res = [1],stack = [0];
+    for (let i = 1; i < n; i++){
+        while(stack.length > 0 && array[stack[stack.length - 1]] <= array[i])stack.pop();
+        stack.length === 0 ? res.push(i+1) : res.push(i-stack[stack.length - 1] )
+        stack.push(i)
+    }
+    console.log(res.join(" "));
+}
+
+const runProgram = (input)=> {
+  input = input.trim().split('\n')
+   let cases = +input[0],line = 1;
+   for (let i = 0; i < cases; i++) {
+       let n = +input[line++],array = input[line++].trim().split(" ").map(Number);
+       findStock(array,n)
+   }
+   
+  }
+  if (process.env.USERNAME === "Dharmesh") {
+    runProgram(`2
+    7
+    100 80 60 70 60 75 85
+    5
+    3 5 0 9 8`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
+  
--- infiniteCurrying.js
+++ infiniteCurrying.js
@@ -1,8 +1,8 @@
-let fun = (a)=>{
-    return (b)=>{
-        if(b )return fun(a+b)
-        return a;
-    }
-}
+let fun = (a) => {
+  return (b) => {
+    if (b) return fun(a + b);
+    return a;
+  };
+};
 
-console.log(fun(1)(2));
+console.log(fun(1)(2)(3)());
