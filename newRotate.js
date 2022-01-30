cf24e103dfb77ba42560170c40cf6c340e8b08d7
Dharmesh <yadavdydv@gmail.com>
2022-01-30 23:21:19 +0530

    update

--- newRotate.js
+++ newRotate.js
@@ -14,4 +14,4 @@ const reverse = (array, i, j) => {
 reverse(array, 0, array.length - (k + 1));
 reverse(array, array.length - k, array.length - 1);
 reverse(array, 0, array.length - 1);
-console.log(...array);
+console.log(...array)
